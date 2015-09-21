425f953bef28a92c228f3ff0ef484cdad3596a5a
Marius Cotofana <marius.coto@gmail.com>
2015-09-21 08:54:12 +0300

    Update mongoose

--- modules/qotd/model.js
+++ modules/qotd/model.js
@@ -2,13 +2,6 @@ var mongoose = require('mongoose')
 var Schema   = mongoose.Schema
 var ObjectId = mongoose.Schema.Types.ObjectId
 
-var Qotd = new Schema({
-  question   : String,
-  date       : Date,
-  choices    : [QOption],
-  answers    : [QResponse],
-  tags 		 : [ObjectId]
-})
 
 var QOption = new Schema({
   text       : String,
@@ -20,6 +13,15 @@ var QResponse = new Schema({
   res        : [QOption],
   date       : Date
 })
+
+var Qotd = new Schema({
+  question   : String,
+  date       : Date,
+  choices    : [QOption],
+  answers    : [QResponse],
+  tags       : [ObjectId]
+})
+
 
 mongoose.model('Qotd', Qotd)
 mongoose.model('QOption', QOption)
