530f2c09578c7def764053315ab48fe05b5444fa
Marius Cotofana <marius.coto@gmail.com>
2015-10-26 22:48:47 +0200

    Replace qotd choises with react code

--- modules/wouso-foundation/components/qotd.jsx
+++ modules/wouso-foundation/components/qotd.jsx
@@ -1,10 +1,21 @@
 
 var QotdOption = React.createClass({
   render: function() {
+    var checkboxDisable = false
+    var spanClass = ""
+
+    // Disable checkboes if questions answered
+    if (this.props.ans) {
+      checkboxDisable = true
+    }
+    // Mark right answers
+    if (this.props.ans == this.props.text)
+      spanClass = "qotd-right-answer"
+
     return (
       <div className="qotd-play-answer">
-        <input type="checkbox" name="ans" disabled></input>
-        <span className="qotd-right-answer">{this.props.text}</span>
+        <input type="checkbox" name="ans" value={this.props.text} disabled={checkboxDisable}></input>
+        <span className={spanClass}>{this.props.text}</span>
       </div>
     )
   }
@@ -13,29 +24,35 @@ var QotdOption = React.createClass({
 var QotdGame = React.createClass({
   getInitialState: function() {
     return {
-      options : []
+      options : [],
+      answer  : ''
     }
   },
 
   componentDidMount: function() {
     $.get('/api/qotd/play', function(result) {
+      console.log(result)
       if (this.isMounted()) {
         this.setState({
-          options: result.answers
+          options : result.answers,
+          answer  : result.answer
         });
       }
     }.bind(this))
   },
 
   render: function() {
+    var answer = this.state.answer
+
     return (
       <div>
         {this.state.options.map(function (opt, i) {
-          return <QotdOption text={opt} />
+          return <QotdOption key={i} text={opt} ans={answer}/>
         })}
       </div>
     )
   }
 })
 
-ReactDOM.render(<QotdGame />, document.getElementById('qotd-play-answers'))
+if( $('#qotd-play-answers').length )
+  ReactDOM.render(<QotdGame />, document.getElementById('qotd-play-answers'))
