917e282a8ba904c148ce7499d2fb70dd6a29fc17
Valentin Mocanu <mrvali97@gmail.com>
2017-03-28 21:35:23 +0300

    Ported from webpack to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,135 @@
-module.exports = {
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var path = require('path');
+
+var exp = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 8090
+  },
+  devtool: 'eval',
   entry: [
-    './node_modules/wouso-foundation/components/wouso-qotd/main.jsx',
-    './node_modules/wouso-foundation/components/wouso-quest/main.jsx',
-    './node_modules/wouso-foundation/components/messages.jsx',
-    './node_modules/wouso-foundation/components/profile.jsx',
-    './node_modules/wouso-foundation/components/admin.jsx'
+    './modules/wouso-foundation/components/wouso-qotd/main.jsx',
+    './modules/wouso-foundation/components/wouso-quest/main.jsx',
+    './modules/wouso-foundation/components/messages.jsx',
+    './modules/wouso-foundation/components/profile.jsx',
+    './modules/wouso-foundation/components/admin.jsx'
   ],
   output: {
-    filename: './node_modules/wouso-foundation/js/bundle.js'
+    path: path.join(__dirname, 'assets'),
+    filename: '[name].js',
+    publicPath: '/assets/'
   },
   module: {
-    loaders: [{
-      //tell webpack to use jsx-loader for all *.jsx files
-      test: /\.jsx$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets:['react']
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                'transform-class-properties',
+                'syntax-async-functions'
+              ],
+              presets: [
+                ['es2017'],
+                'react'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              minetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              minetype: 'image/jpg'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.gif$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              minetype: 'image/gif'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+        use: 'url-loader'
       }
-    },
-    {
-      //tell webpack to use jsx-loader for all *.jsx files
-      test: /\.json$/,
-      loader: 'json-loader',
-      exclude: /node_modules/,
-    }]
+    ]
   },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer(),
+          precss()
+        ]
+      }
+    })
+  ],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    modules: [
+      path.resolve(__dirname, '/modules'),
+      'node_modules'
+    ],
+    alias: {
+      config: path.resolve(__dirname, `config.js`)
+    },
+    extensions: ['.js', '.jsx']
+  },
+  performance: {
+    hints: false
   }
 }
+
+module.exports = exp;
