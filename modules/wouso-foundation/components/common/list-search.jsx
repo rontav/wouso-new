121cd82dcab16b3f4aa4275538d469d4b884022c
ursescuionut <ursescuionut97@gmail.com>
2017-03-28 21:42:47 +0300

    Refactoring to es2017(WIP)

--- modules/wouso-foundation/components/common/list-search.jsx
+++ modules/wouso-foundation/components/common/list-search.jsx
@@ -1,65 +1,79 @@
-var React         = require('react');
-var AppDispatcher = require('../../dispatchers/app');
+import React from 'react';
+import AppDispatcher from '../../dispatchers/app';
 
 
-var ListSearch = React.createClass({
-  handleDeleteClick: function() {
+class ListSearch extends React.Component {
+
+  constructor() {
+    super();
+    this.gotResponse = this.gotResponse.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleClearClick = this.handleClearClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  // static propTypes = {
+  // selected: React.PropTypes,
+  // searchType: React.PropTypes,
+  // refreshType : React.PropTypes,
+  // }
+
+  gotResponse() {
+    AppDispatcher.handleViewAction({
+      type: refreshType
+    });
+  }
+  handleDeleteClick() {
     if (this.props.selected.length == 0) {
       alert('First select the questions that you need to delete.');
     } else {
-      var refreshType = this.props.refreshType;
+      //var refreshType = this.props.refreshType; ?? 
       var conf = confirm('Are you sure you want to permanently delete selected questions?');
+
       if (conf) {
         $.ajax({
-          type    : "DELETE",
-          url     : '/api/wouso-quest/delete?id=' + this.props.selected.join(','),
-          data    : null,
-          success : gotResponse
-        });
-      }
-
-      function gotResponse(res) {
-        AppDispatcher.handleViewAction({
-          type: refreshType
+          type: "DELETE",
+          url: '/api/wouso-quest/delete?id=' + this.props.selected.join(','),
+          data: null,
+          success: gotResponse
         });
       }
     }
-  },
+  }
 
-  handleClearClick: function() {
+  handleClearClick() {
     AppDispatcher.handleViewAction({
-      type : this.props.searchType,
-      term : ''
+      type: this.props.searchType,
+      term: ''
     });
     AppDispatcher.handleViewAction({
-      type : this.props.refreshType
+      type: this.props.refreshType
     });
     this.clearButton.value = '';
-  },
+  }
 
-  handleChange: function(event) {
+  handleChange(event) {
     AppDispatcher.handleViewAction({
-      type : this.props.searchType,
-      term : String(event.target.value)
+      type: this.props.searchType,
+      term: String(event.target.value)
     });
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <div>
         <a className="radius button" href="#"
-           onClick={this.handleDeleteClick}>Delete</a>
+          onClick={this.handleDeleteClick}>Delete</a>
         <div className="row">
           <div className="large-12 columns">
             <div className="row collapse">
               <div className="small-4 columns">
                 <input type="text" ref={(ref) => this.clearButton = ref}
-                       onChange={this.handleChange} placeholder="Search">
+                  onChange={this.handleChange} placeholder="Search">
                 </input>
               </div>
               <div className="small-1 columns">
                 <a href="#" className="button postfix"
-                   onClick={this.handleClearClick}>Clear</a>
+                  onClick={this.handleClearClick}>Clear</a>
               </div>
               <div className="small-7 columns"></div>
             </div>
@@ -68,6 +82,6 @@ var ListSearch = React.createClass({
       </div>
     );
   }
-});
+};
 
 module.exports = ListSearch;
